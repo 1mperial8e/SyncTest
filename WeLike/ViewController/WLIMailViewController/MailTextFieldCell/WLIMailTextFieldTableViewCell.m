538aad0856bb741121eaa6d3d58747830bec19b6
Stas Volskyi <stas.volskiy@thinkmobiles.com>
2015-12-08 17:17:13 +0200

    mailVC fixed

--- WeLike/ViewController/WLIMailViewController/MailTextFieldCell/WLIMailTextFieldTableViewCell.m
+++ WeLike/ViewController/WLIMailViewController/MailTextFieldCell/WLIMailTextFieldTableViewCell.m
@@ -2,7 +2,7 @@
 //  WLIMailTextFieldTableViewCell.m
 //  MyDrive
 //
-//  Created by Roman R on 08.12.15.
+//  Created by Stas Volskyi on 08.12.15.
 //  Copyright © 2015 Goran Vuksic. All rights reserved.
 //
 
