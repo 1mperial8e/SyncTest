eb7ab1072b5276921c6a92b1674757a2dddeacc1
Stas Volskyi <stas.volskiy@thinkmobiles.com>
2015-12-16 18:59:31 +0200

    added what is new view controller

---
+++ WeLike/ViewController/WhatIsNewController/WLIWhatIsNewViewController.h
@@ -0,0 +1,11 @@
+//
+//  WLIWhatIsNewViewController.h
+//  MyDrive
+//
+//  Created by Stas Volskyi on 12/16/15.
+//  Copyright © 2015 Goran Vuksic. All rights reserved.
+//
+
+@interface WLIWhatIsNewViewController : UIViewController
+
+@end
